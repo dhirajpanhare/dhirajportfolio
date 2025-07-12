1d89ebde6bfdf20cd1b060e02de3df18458b8855
exact <dhirajpanhare08@gmail.com>
2025-07-12 14:06:09 +0530

    code commit

--- Dhirajpanhare/src/Pages/Home.jsx
+++ Dhirajpanhare/src/Pages/Home.jsx
@@ -1,22 +1,21 @@
 import Navabr from "../components/Navbar";
-import Hero from "../components/Hero.Jsx";
+import Hero from "../components/Hero";
 import About_Section from "../components/About-Section";
 import Project_Section from "../components/Project_Section";
 import Certificate_Section from "../components/Certificate-Section";
 import Contact_Section from "../components/Contact-Section";
 import Notes_Section from "../components/Notes-Section";
 
-function Home()
-{
+function Home() {
     return (
         <>
-         <Navabr/>
-            <Hero/>
-         <Project_Section/>
-            <About_Section/> 
-            <Certificate_Section/>
-            <Contact_Section/>
-            <Notes_Section/>
+            <Navabr />
+            <Hero />
+            <Project_Section />
+            <About_Section />
+            <Certificate_Section />
+            <Contact_Section />
+            <Notes_Section />
         </>
     )
 }
